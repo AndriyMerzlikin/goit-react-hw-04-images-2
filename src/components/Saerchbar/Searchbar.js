45c045a26c647ac90019028949cebfd2e9a20362
AndriyMerzlikin <lastheroofme@gmail.com>
2023-11-15 13:46:12 +0200

    Update Searchbar.js

--- src/components/Saerchbar/Searchbar.js
+++ src/components/Saerchbar/Searchbar.js
@@ -1,3 +1,50 @@
+// import { AiOutlineSearch } from 'react-icons/ai';
+// import { Formik, Field, Form } from 'formik';
+// import {
+//   SearchForm,
+//   SearchFormButton,
+//   SearchbarContainer,
+//   SearchFormInput,
+// } from './Searchbar.styled';
+
+// export const Searchbar = ({ addGalery }) => {
+//   return (
+//     <SearchbarContainer>
+//       <Formik
+//         initialValues={{
+//           queryName: '',
+//         }}
+//         onSubmit={async values => {
+//           await addGalery(values.queryName);
+//         }}
+//       >
+//         <Form>
+//           <label htmlFor="queryName"></label>
+//           <Field id="queryName" name="queryName" placeholder="Search" />
+
+//           <button type="submit">Search</button>
+//         </Form>
+//       </Formik>
+
+//       {/* <SearchForm
+//         onSubmit={async values => {
+//           await addGalery(values.queryName);
+//         }}
+//       >
+//         <SearchFormButton type="submit">
+//           <AiOutlineSearch size="2em" />
+//         </SearchFormButton>
+
+//         <SearchFormInput
+//           type="text"
+//           //   autocomplete="off"
+//           //   autofocus
+//           placeholder="Search images and photos"
+//         />
+//       </SearchForm> */}
+//     </SearchbarContainer>
+//   );
+// };
 import { AiOutlineSearch } from 'react-icons/ai';
 import { Formik, Field, Form } from 'formik';
 import {
@@ -14,34 +61,24 @@ export const Searchbar = ({ addGalery }) => {
         initialValues={{
           queryName: '',
         }}
-        onSubmit={async values => {
+        onSubmit={async (values, { setFieldValue }) => {
           await addGalery(values.queryName);
+          // Скидання значення інпута після відправлення форми
+          setFieldValue('queryName', '');
         }}
       >
         <Form>
           <label htmlFor="queryName"></label>
-          <Field id="queryName" name="queryName" placeholder="Search" />
+          <Field
+            id="queryName"
+            name="queryName"
+            placeholder="Search"
+            component="input"
+          />
 
           <button type="submit">Search</button>
         </Form>
       </Formik>
-
-      {/* <SearchForm
-        onSubmit={async values => {
-          await addGalery(values.queryName);
-        }}
-      >
-        <SearchFormButton type="submit">
-          <AiOutlineSearch size="2em" />
-        </SearchFormButton>
-
-        <SearchFormInput
-          type="text"
-          //   autocomplete="off"
-          //   autofocus
-          placeholder="Search images and photos"
-        />
-      </SearchForm> */}
     </SearchbarContainer>
   );
 };
